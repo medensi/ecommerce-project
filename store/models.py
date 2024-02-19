8a9970fbd0b0c87811d29f90341078c03f43096b
mecom <example@mail.com>
2024-02-19 23:00:04 +0400

    rendered products, added names, modified store file with for loop, modified models file

--- store/models.py
+++ store/models.py
@@ -37,4 +37,12 @@ class OrderItem(models.Model):
 
 class ShippingAddress(models.Model):
    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True) #if order gets deleted, we still have shipping address of the customer
-   customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+   order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+   address = models.CharField(max_length=200, null=True)
+   city = models.CharField(max_length=200, null=True)
+   state = models.CharField(max_length=200, null=True)
+   zipcode = models.CharField(max_length=200, null=True)
+   date_added = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self) -> str:
+      return self.address
